1c0bb2ac6c4c70bef98217770609e99a42dd2377
Pavan sai <pavanaws57@gmail.com>
2024-09-30 13:02:43 +0530

    updates

---
+++ eks-terraform/backend.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.67.0"
+    }
+  }
+  backend "s3" {
+    bucket = "hjksdfgwertyuvbnpoqiuhxb"
+    key = "eks/terraform.tfstate"
+    region = "ap-south-1"
+  }
+  required_version = ">= 1.6.3"
+}
